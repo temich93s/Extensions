481da9fb17923c60afbaad482f5d3cd15b33eb87
Temich93s <nil>
2021-10-09 12:06:19 +1000

    Пройдена тема: Вычисляемые свойства в расширениях

--- Extensions/main.swift
+++ Extensions/main.swift
@@ -9,3 +9,23 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Вычисляемые свойства в расширениях
+print("\n//Вычисляемые свойства в расширениях")
+
+extension Double {
+    var km: Double { self * 1000 }
+    var  m: Double { self }
+    var cm: Double { self / 100.0 }
+    var mm: Double { self / 1_000.0 }
+    var ft: Double { self / 3.28084 }
+}
+
+let oneInch = 25.4.mm
+print("Один дюйм - это \(oneInch) метра")
+// Выведет "Один дюйм- это 0.0254 метра"
+
+let threeFeet = 3.ft
+print("Три фута - это \(threeFeet) метра")
+// Выведет "Три фута - это 0.914399970739201 метра"
+
