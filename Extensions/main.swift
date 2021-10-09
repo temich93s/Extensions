20dedfa9e62f1eec7fda01214abff98ba2735f31
Temich93s <nil>
2021-10-09 12:25:07 +1000

    Пройдена тема: Инициализаторы в расширениях

--- Extensions/main.swift
+++ Extensions/main.swift
@@ -29,3 +29,35 @@ let threeFeet = 3.ft
 print("Три фута - это \(threeFeet) метра")
 // Выведет "Три фута - это 0.914399970739201 метра"
 
+
+//MARK: Инициализаторы в расширениях
+print("\n//Инициализаторы в расширениях")
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+print(defaultRect)
+print(memberwiseRect)
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+// исходная точка centerRect (2.5, 2.5) и его размер (3.0, 3.0)
+print(centerRect)
+
